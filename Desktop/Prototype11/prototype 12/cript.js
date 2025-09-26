87dca1d3ecf9222e19600185684fb1f2858289ae
yassminaganga <gangayassmina@gmail.com>
2025-09-26 11:14:08 +0100

    ajouter les chapitres 12 et 13

---
+++ Desktop/Prototype11/prototype 12/cript.js
@@ -0,0 +1,186 @@
+let codes = ["C3", "C4","C7", "C3", "C4","C7", "C3", "C4","C7"];
+let count = 0 ;
+let  compteur = "C4";
+
+for (let i = 0; i < codes.length; i++){
+    if (codes[i] ===  compteur) {
+        count++;
+      }
+}
+  
+    console.log("compteur" +count );
+
+// // for (let i = 0; i < codes.length; i++) {
+// //   if (codes[i] === compteur) {
+// //     count++;
+// //   }
+    
+// // }
+
+// // console.log("compteur" +count);
+
+
+
+
+  
+   
+
+
+
+
+
+
+// // for (let i = 0; i < codes.length; i++) {
+// //  if(codes[i] === compteur){
+// //     count++;
+// //  }
+    
+// // }
+// //   console.log("compteur" +codes) ;
+// // let count = 0;
+// // let compteur = "C3";
+// // for (let i = 0; i < codes.length; i++) {
+// //   if (codes[i] === compteur) {
+// //     count++;
+// //   }
+    
+// // }
+
+// // console.log("compteur" +count);
+// // let count = 0;
+// // let compteur = "C3";
+// // for (let i = 0; i < codes.length; i++) {
+// //     if (codes[i] === compteur) {
+// //        count++; 
+// //     }
+    
+// // }
+
+// // console.log("compteur" +count);
+
+
+
+// // let count = 0;
+// // let compteur = "C3";
+// // for (let i = 0; i < codes.length; i++) {
+// //     if (codes[i] ===compteur) {
+// //         count++;
+        
+// //     }
+    
+// // }
+
+// // console.log("comptur" +codes);
+
+
+
+
+
+
+
+
+// // for (let i = 0; i < codes.length; i++) {
+// //  if  ( codes[i] === compteur) {
+// // count++;
+// //    }
+    
+// // }
+// // console.log("compteur" +count);
+// // for (let i = 0; i < codes.length; i++) {
+// //    if (codes[i] === compteur) {
+// //     count++;
+  
+// // }  
+// // }
+
+// // console.log("compteur" +count);
+
+// // for (let i = 0; i < codes.length; i++) {
+// //   if  (codes[i] === compteur) {
+// //     count++;
+// //   }
+    
+// // }
+// // console.log("compteur" +count);
+
+
+
+
+
+
+
+
+
+// // for (let i = 0; i < array.length; i++) {
+// //   if (codes[i] === compteur) {
+// //     count++;
+// //   }
+    
+// // }
+// // console.log("compteur" +count);
+
+// // for (let i = 0; i < array.length; i++) {
+// //  if  (codes[i]=== compteur){
+// //     count ++;
+// //  }
+    
+// // }
+// // console.log("compteur" +count)
+
+
+
+
+
+
+
+
+
+
+// // for (let i = 0; i < codes.length; i++){
+// //     if (codes[i] === compteur ){
+// //         count++;
+// //     }
+// // }
+
+// // console.log("compteur" +count);
+
+
+
+// // for (let i = 0; i < codes.length; i++) {
+// //    if (codes[i] === compteur) {
+// //     count++;
+// //    }
+
+// //    console.log("copteur" +count);
+    
+// // }
+
+
+
+// // function estAnagramme(str1, str2) {
+  
+//     str1 = str1.replace(/\s/g, "").toLowerCase();
+//     str2 = str2.replace(/\s/g, "").toLowerCase();
+  
+  
+//     if (str1.length !== str2.length) return false;
+  
+  
+//     let mot1 = str1.split("").sort().join("");
+//     let mot2 = str2.split("").sort().join("");
+  
+   
+//     return mot1 === mot2;
+//   }
+  
+ 
+//   console.log(estAnagramme("anagram", "nagaram")); 
+  
+
+
+// // function estAnagramme(a, b) {
+// //     return a.replace(/\s/g,"").toLowerCase().split("").sort().join("") === 
+// //            b.replace(/\s/g,"").toLowerCase().split("").sort().join("");
+// //   }
+  
+// //   console.log(estAnagramme("anagram", "nagaram"));
